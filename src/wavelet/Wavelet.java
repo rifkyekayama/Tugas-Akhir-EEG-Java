7101aafcdc366f22398d72d7a4797b76569f5d44
rifqykiw <rifqykiw@gmail.com>
2016-01-09 13:12:20 +0700

    add mysql-connector

--- src/wavelet/Wavelet.java
+++ src/wavelet/Wavelet.java
@@ -4,8 +4,6 @@ import java.awt.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 import mysql.DatabaseAction;
 
@@ -378,7 +376,6 @@ public class Wavelet {
 			}
 			neuron[i][1] = objectKelas;
 			iTemp=0;
-			System.out.println(neuron[i][0][0]);
 		}
 		
 		return neuron;
